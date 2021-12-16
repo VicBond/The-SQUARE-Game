31632bf8de08bf7fa78655de05deb12a14531ff2
Victor Bondarenko <nirasimha@gmail.com>
2021-12-16 09:46:55 -0500

    random position of square

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable func-style */
 let $start = document.querySelector('#start');
 let $game = document.querySelector('#game');
 
@@ -22,14 +24,24 @@ function handleBoxClick(event) {
 function renderBox() {
   $game.innerHTML = '';
   let box = document.createElement('div');
+  // console.log(getRandom(30, 100));
+  let boxSize = getRandom(30, 100);
+  let gameSize = $game.getBoundingClientRect();
+  // console.log(gameSize);
+  let maxTop = gameSize.height - boxSize;
+  let maxLeft = gameSize.width - boxSize;
 
-  box.style.height = box.style.width = '50px';
+  box.style.height = box.style.width = `${boxSize}px`;
   box.style.position = 'absolute';
   box.style.backgroundColor = '#000';
-  box.style.top = '50px';
-  box.style.left = '70px';
+  box.style.top = `${getRandom(0, maxTop)}px`;
+  box.style.left = `${getRandom(0, maxLeft)}px`;
   box.style.cursor = 'pointer';
   box.setAttribute('data-box', 'true');
 
   $game.insertAdjacentElement('afterbegin', box);
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
