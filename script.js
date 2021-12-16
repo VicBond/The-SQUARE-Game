018d98b43c0f717f8a8c6aa86750324ed60fd675
Victor Bondarenko <nirasimha@gmail.com>
2021-12-16 09:34:38 -0500

    render square added

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 let $start = document.querySelector('#start');
 let $game = document.querySelector('#game');
 
+let score = 0;
+
 $start.addEventListener('click', startGame);
+$game.addEventListener('click', handleBoxClick);
 
 function startGame() {
   $game.style.backgroundColor = '#fff';
   $start.classList.add('hide');
+  score ++;
+  renderBox();
+}
 
+function handleBoxClick(event) {
+  if (event.target.dataset.box) {
+    renderBox();
+  }
+}
+
+function renderBox() {
+  $game.innerHTML = '';
+  let box = document.createElement('div');
+
+  box.style.height = box.style.width = '50px';
+  box.style.position = 'absolute';
+  box.style.backgroundColor = '#000';
+  box.style.top = '50px';
+  box.style.left = '70px';
+  box.style.cursor = 'pointer';
+  box.setAttribute('data-box', 'true');
+
+  $game.insertAdjacentElement('afterbegin', box);
 }
