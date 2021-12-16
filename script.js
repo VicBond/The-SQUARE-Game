128d2724515568428e70413129831b5a0473bf0b
Victor Bondarenko <nirasimha@gmail.com>
2021-12-16 08:29:45 -0500

    script created start game func added

---
+++ script.js
@@ -0,0 +1,10 @@
+let $start = document.querySelector('#start');
+let $game = document.querySelector('#game');
+
+$start.addEventListener('click', startGame);
+
+function startGame() {
+  $game.style.backgroundColor = '#fff';
+  $start.classList.add('hide');
+
+}
